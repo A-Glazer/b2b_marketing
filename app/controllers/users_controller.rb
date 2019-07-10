c58e9ae127941e7e83f373a51921ff8fb557568d
imaglazer <avigayilglazer@gmail.com>
2019-07-10 21:52:33 +0300

    created sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
     #loading the signup form
     def new
+        @user = User.new
     end
 
     #signup
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_many :business_services 
     has_many :potential_clients, through: :business_services
+
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: true
 end
