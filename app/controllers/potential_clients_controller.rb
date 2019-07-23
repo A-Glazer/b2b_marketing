158bce8fa24ccd4af3bfab1df443da6485ca62ec
imaglazer <avigayilglazer@gmail.com>
2019-07-23 20:27:51 +0300

    cleaned up potential client controller

--- app/controllers/potential_clients_controller.rb
+++ app/controllers/potential_clients_controller.rb
@@ -2,34 +2,21 @@ class PotentialClientsController < ApplicationController
     before_action :logged_in?, :current_user
 
     def new
-        @business_service = BusinessService.find_by(id: params[:business_service_id])
-   
-        # @potential_client = PotentialClient.new
-    
-        # binding.pry
+        find_service
         if params[:business_service_id] && !BusinessService.exists?(params[:business_service_id])
-            redirect_to business_services_path, alert: "Business service not found."
+            redirect_to business_services_path
         else
             @potential_client = PotentialClient.new(business_service_id: params[:business_service_id])
         end
     end
 
     def create 
-        # binding.pry
-        # raise params.inspect
-        @business_service = BusinessService.find_by(id: params[:business_service_id])
-        # @business_service = BusinessService.find(params[:business_service_id])
-        # @potential_client = @business_service.build_potential_client(client_params)
-        @potential_client = PotentialClient.new(client_params)
-        # @potential_client.business_service_id = @business_service.id
-        # @potential_client.user_id = current_user.id
+        find_service
+        @potential_client = PotentialClient.new(client_params)   
         
         if @potential_client
             @potential_client.business_services << @business_service unless @potential_client.business_services.include?(@business_service)
-            # binding.pry
             @potential_client.save
-            # @business_service.save
-            # binding.pry
             redirect_to business_service_potential_client_path(@business_service.id, @potential_client)
         else
             render :new
@@ -38,28 +25,25 @@ class PotentialClientsController < ApplicationController
 
 
     def show
-        @potential_client = PotentialClient.find_by(id: params[:id])
-        #binding.pry
+        @potential_client = PotentialClient.find_client(params).last #scope being called
+        # @potential_client = PotentialClient.find_by(id: params[:id])
         @business_service = @potential_client.business_service_id
-        # @business_service = BusinessService.find_by(params[:business_service_id])
-        # binding.pry
-  
     end
 
     def index
         @potential_clients = PotentialClient.all
-        @potential_client = PotentialClient.find_by(id: params[:id])
-        @business_service = BusinessService.find_by(id: params[:business_service_id])
+        @potential_client = PotentialClient.find_client(params).last
+        # @potential_client = PotentialClient.find_by(id: params[:id])
+        find_service
     end
 
     def edit
-        @business_service = BusinessService.find_by(id: params[:business_service_id])
+        find_service
         @potential_client = PotentialClient.find_client(params).last
-        #binding.pry
     end
 
     def update
-        @business_service = BusinessService.find_by(id: params[:business_service_id])
+        find_service
         @potential_client = PotentialClient.find_client(params).last
         if @potential_client.update(client_params)
             redirect_to business_service_potential_client_path(@business_service, @potential_client)
@@ -84,14 +68,7 @@ class PotentialClientsController < ApplicationController
         @created_before = self.created_before(date)
     end
 
-    # def find_service
-    #     @business_service = BusinessService.find_by(id: params[:id])
-    # end
-
-    # def find_client2
-    #     @potential_client = PotentialClient.find_by(id: params[:id])
-    #     if !@potential_client
-    #         redirect_to business_service_potential_clients_path(@business_service)
-    #     end
-    # end
+    def find_service
+        @business_service = BusinessService.find_by(id: params[:business_service_id])
+    end
 end
