dcfc4ec3063d83201b75aef2d5ad18111517f1e2
imaglazer <avigayilglazer@gmail.com>
2019-07-24 13:26:58 +0300

    removed pseudocode for all duplicate links in the navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,16 +6,16 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
-
+    
     #signup
     def create
         @user = User.new(user_params)
-        if @user.save
-            session[:user_id] = @user.id
-            redirect_to user_path(@user)
-        else
-            render :new
-        end
+            if @user.save
+                session[:user_id] = @user.id
+                redirect_to user_path(@user)
+            else
+                render :new
+            end
     end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
     has_many :potential_clients, through: :business_services
 
     validates :username, presence: true, uniqueness: true
+    validates :password_confirmation, presence: true
 
     def self.find_or_create_by_omniauth(auth)
         self.where(uid: auth['uid']).first_or_create do |user|
