a5443550981a60742effe63a08d6147a06641f9e
A-Glazer <avigayilglazer@gmail.com>
2019-10-02 11:55:49 +0300

    reverted changes for sessions and users controller, and their view pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,50 +1,42 @@
 class SessionsController < ApplicationController
 
-    def index
-        @business_services = BusinessService.all
-        respond_to do |f|
-            f.html {render :index}
-            f.json {render json: @business_service}
+    def new
+        @user = User.new
+        render :login    
+    end
+    
+    def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to business_services_path
+        else
+            #use redirect here vs render to avoid errors in the form_for
+            flash[:error] = "Sorry, your username or password was incorrect."
+            redirect_to '/login'
         end
     end
 
-#     def new
-#         @user = User.new
-#         render :login    
-#     end
-    
-#     def create
-#         @user = User.find_by(username: params[:user][:username])
-#         if @user && @user.authenticate(params[:user][:password])
-#             session[:user_id] = @user.id
-#             redirect_to business_services_path
-#         else
-#             #use redirect here vs render to avoid errors in the form_for
-#             flash[:error] = "Sorry, your username or password was incorrect."
-#             redirect_to '/login'
-#         end
-#     end
+    #omniauth facebook login
+    def facebook_create
+        @user = User.find_or_create_by_omniauth(auth)       
+        session[:user_id] = @user.id
+        redirect_to business_services_path
+    end
 
-#     #omniauth facebook login
-#     def facebook_create
-#         @user = User.find_or_create_by_omniauth(auth)       
-#         session[:user_id] = @user.id
-#         redirect_to business_services_path
-#     end
+    def home
+    end
 
-#     def home
-#     end
+    #logout
+    def destroy
+        session.clear
+        redirect_to '/'
+    end
 
-#     #logout
-#     def destroy
-#         session.clear
-#         redirect_to '/'
-#     end
-
-#     private
+    private
  
-#   def auth
-#     request.env['omniauth.auth']
-#   end
+  def auth
+    request.env['omniauth.auth']
+  end
 
 end
